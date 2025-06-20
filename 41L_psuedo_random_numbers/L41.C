0f04cadc50eac76eb2b181803229bfb9af71f69a
windylyswifts <isiahalinab@gmail.com>
2025-06-20 17:47:01 +0800

    Revised comments for future use

--- 41L_psuedo_random_numbers/L41.C
+++ 41L_psuedo_random_numbers/L41.C
@@ -7,7 +7,7 @@ int main()
     //pseudo random numbers = A set of values or elements that are statistically random
     //                       (Don't use these for any sort of crypotographic security)
 
-    srand(time(0)); //The data type srand meaning, 's' for seed and "rand" is random.
+    srand(time(0)); //The data type srand meaning, 's' for seed and "rand" is random. Usually 0 means NULL like \n or \0
  
     int number1 = (rand() % 6) + 1;//the rand function will give you a random number 0 to 32,76. the 
                                    //the symbol '%' is modulus and it gives you the remainder of
